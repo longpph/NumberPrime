ca4f5da23dc45c2e34ffdf13abab5887018754df
longpph <long.pham2@student.codegym.vn>
2026-06-24 16:16:20 +0700

    Update 100 prime

--- NumberPrime.java
+++ NumberPrime.java
@@ -15,10 +15,18 @@ public class NumberPrime {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int numbers = scanner.nextInt();
         int count = 0;
         int N = 2;
+        System.out.print("Các số nguyên tố nhỏ hơn 100: ");
+        for (int i = 2; i < 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("Enter number: ");
+        int numbers = scanner.nextInt();
+        System.out.printf("%d số nguyên tố đầu tiên: ", numbers);
         while (count < numbers) {
             if (isPrime(N)) {
                 System.out.print(N + " ");
